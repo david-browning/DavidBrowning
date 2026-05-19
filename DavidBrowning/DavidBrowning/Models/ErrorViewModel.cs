1688e6658409949c7a0921ab0367eb783fe45643
David Browning <davidbr101@hotmail.com>
2026-05-18 18:03:59 -0700

    Add inital website core and styling.

---
+++ DavidBrowning/DavidBrowning/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+// Copyright © 2026 David Browning. All rights reserved.
+// Source-available for viewing only. No license granted.
+
+namespace DavidBrowning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DavidBrowning/DavidBrowning/wwwroot/js/site.js
@@ -0,0 +1,2 @@
+﻿// Copyright © 2026 David Browning. All rights reserved.
+// Source-available for viewing only. No license granted.
